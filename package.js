d3acbcd218dec7513d8d0a5e904b6599344c57b3
Brent Abrahams <brent_abrahams@yahoo.com>
2018-09-16 07:37:15 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   name: 'babrahams:accounts-ldap-ionic',
-  version: '0.4.7',
+  version: '0.4.8',
   summary: 'Gives babrahams:accounts-ldap package a form in the Ionic style',
   git: 'https://github.com/JackAdams/meteor-accounts-ldap-ionic',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.1.0.2');
   api.use(['templating', 'underscore', 'less@2.7.8', 'sacha:spin@2.3.1'], 'client');
-  api.use('babrahams:accounts-ldap@0.7.7');
+  api.use('babrahams:accounts-ldap@0.7.8');
   api.addFiles('accounts-ldap-ionic.css', 'client');
   api.addFiles('accounts-ldap-ionic.html', 'client');
   api.addFiles('accounts-ldap-ionic.js', 'client');
   api.imply('babrahams:accounts-ldap');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('babrahams:accounts-ldap-ionic');
   api.addFiles('accounts-ldap-ionic-tests.js');
