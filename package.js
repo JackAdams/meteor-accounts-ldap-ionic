961c77775c52d0cd4cdc6a4ef9d5fed0a6610684
Brent Abrahams <brent_abrahams@yahoo.com>
2022-07-30 15:06:30 +0800

    Version bump

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom(['1.8.2', '2.3']);
   api.use(['templating@1.3.2', 'underscore', 'less@3.0.1', 'sacha:spin@2.3.1'], 'client');
-  api.use('babrahams:accounts-ldap@0.9.9');
+  api.use('babrahams:accounts-ldap@0.9.10');
   api.addFiles('accounts-ldap-ionic.css', 'client');
   api.addFiles('accounts-ldap-ionic.html', 'client');
   api.addFiles('accounts-ldap-ionic.js', 'client');
